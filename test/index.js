60d9df4ece863795e548b05df9a0e3b6ba49ae08
Julian Viereck <julian.viereck@gmail.com>
2013-11-03 21:32:43 +0100

    Change indention to 2-space and bump version to 0.0.2

--- test/index.js
+++ test/index.js
@@ -6,27 +6,27 @@ var parseTests = JSON.parse(fs.readFileSync('test/parse_input.json') || '[]');
 var parseResult = JSON.parse(fs.readFileSync('test/parse_output.json') || '[]');
 
 if (parseTests.length !== parseResult.length) {
-    fail('Parse input and output file needs to have same number of arguments');
+  fail('Parse input and output file needs to have same number of arguments');
 }
 
 parseTests.forEach(function(input, idx) {
-    var par;
-    try {
-        par = parse(input);
-    } catch (error) {
-        par = {
-            type: 'error',
-            name: error.name,
-            message: error.message,
-            input: input
-        };
-    }
+  var par;
+  try {
+    par = parse(input);
+  } catch (error) {
+    par = {
+      type: 'error',
+      name: error.name,
+      message: error.message,
+      input: input
+    };
+  }
 
-    var resuls = parseResult[idx];
+  var resuls = parseResult[idx];
 
-    if (JSON.stringify(par) !== JSON.stringify(resuls)) {
-        throw new Error('Failure parsing string ' + input + ':' + JSON.stringify(par) + '\n' + JSON.stringify(resuls));
-    } else {
-        console.log('PASSED TEST: ' + input);
-    }
+  if (JSON.stringify(par) !== JSON.stringify(resuls)) {
+    throw new Error('Failure parsing string ' + input + ':' + JSON.stringify(par) + '\n' + JSON.stringify(resuls));
+  } else {
+    console.log('PASSED TEST: ' + input);
+  }
 });
