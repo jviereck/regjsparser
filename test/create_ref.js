a4afc79f24d40ebc4a82e8c99d1a2976ab01b408
Julian Viereck <julian.viereck@gmail.com>
2014-04-07 14:20:41 +0200

    Update the test/create_ref.js file and generate new test input/output files

--- test/create_ref.js
+++ test/create_ref.js
@@ -2,20 +2,26 @@ var fs = require('fs');
 
 var parse = require('../parser').parse;
 
-var parseTests = JSON.parse(fs.readFileSync('test/parse_input.json') || '[]');
+function mapInput(inputFile, outputFile, flags) {
+  var parseTests = JSON.parse(fs.readFileSync(inputFile) || '[]');
 
-var arr = parseTests.map(function(input) {
-  var result;
-  try {
-    result = parse(input);
-  } catch (error) {
-    result = {
-      type: 'error',
-      name: error.name,
-      message: error.message,
-      input: input
-    };
-  }
-  return result;
-});
-fs.writeFileSync('test/parse_output.json', JSON.stringify(arr, null, 2));
+  var arr = parseTests.map(function(input) {
+    var result;
+    try {
+      result = parse(input, flags);
+    } catch (error) {
+      result = {
+        type: 'error',
+        name: error.name,
+        message: error.message,
+        input: input
+      };
+    }
+    return result;
+  });
+  fs.writeFileSync(outputFile, JSON.stringify(arr, null, 2));
+}
+
+mapInput('test/parse_input.json', 'test/parse_output.json', '');
+mapInput('test/parse_unicode_input.json', 'test/parse_unicode_output.json', 'u');
+
