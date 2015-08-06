9001dabf500053acc34e3b5084d3cd10fed5db18
kpdecker <kpdecker@gmail.com>
2015-08-06 11:21:06 -0500

    Update var style per review

--- parser.js
+++ parser.js
@@ -899,14 +899,14 @@
       from = from == null ? pos : from;
       to = to == null ? from : to;
 
-      var contextStart = Math.max(0, from - 10),
-          contextEnd = Math.min(to + 10, str.length);
+      var contextStart = Math.max(0, from - 10);
+      var contextEnd = Math.min(to + 10, str.length);
 
       // Output a bit of context and a line pointing to where our error is.
       //
       // We are assuming that there are no actual newlines in the content as this is a regular expression.
-      var context = '    ' + str.substring(contextStart, contextEnd),
-          pointer = '    ' + new Array(from - contextStart + 1).join(' ') + '^';
+      var context = '    ' + str.substring(contextStart, contextEnd);
+      var pointer = '    ' + new Array(from - contextStart + 1).join(' ') + '^';
 
       throw SyntaxError(message + ' at position ' + from + '\n' + context + '\n' + pointer);
     }
