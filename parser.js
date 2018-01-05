3f1af8d15a7ec140d757b1e58a0e99055382dd9b
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-01-05 18:43:13 +0100

    Full polyfill

--- parser.js
+++ parser.js
@@ -144,19 +144,51 @@
 
 (function() {
 
-  // Simplified implementation of String.fromCodePoint for legacy environments.
-  // This function doesn't check that the parameter is a positive integer
-  // and <= 0x10ffff, since it will only be called with valid codepoints.
-  var fromCodePoint = String.fromCodePoint || function(codepoint) {
-    if (codepoint <= 0xffff) { // BMP
-      return String.fromCharCode(codepoint);
-    } else { // astral
-      var cp = codepoint - 0x10000;
-      var highSurrogate = (cp >> 10) + 0xd800;
-      var lowSurrogate = (cp % 1024) + 0xdc00;
-      return String.fromCharCode(highSurrogate, lowSurrogate);
-    }
-  };
+  var fromCodePoint = String.fromCodePoint || (function() {
+    // Implementation taken from
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCodePoint
+
+    var stringFromCharCode = String.fromCharCode;
+    var floor = Math.floor;
+
+    return function fromCodePoint() {
+      var MAX_SIZE = 0x4000;
+      var codeUnits = [];
+      var highSurrogate;
+      var lowSurrogate;
+      var index = -1;
+      var length = arguments.length;
+      if (!length) {
+        return '';
+      }
+      var result = '';
+      while (++index < length) {
+        var codePoint = Number(arguments[index]);
+        if (
+          !isFinite(codePoint) ||       // `NaN`, `+Infinity`, or `-Infinity`
+          codePoint < 0 ||              // not a valid Unicode code point
+          codePoint > 0x10FFFF ||       // not a valid Unicode code point
+          floor(codePoint) != codePoint // not an integer
+        ) {
+          throw RangeError('Invalid code point: ' + codePoint);
+        }
+        if (codePoint <= 0xFFFF) { // BMP code point
+          codeUnits.push(codePoint);
+        } else { // Astral code point; split in surrogate halves
+          // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+          codePoint -= 0x10000;
+          highSurrogate = (codePoint >> 10) + 0xD800;
+          lowSurrogate = (codePoint % 0x400) + 0xDC00;
+          codeUnits.push(highSurrogate, lowSurrogate);
+        }
+        if (index + 1 == length || codeUnits.length > MAX_SIZE) {
+          result += stringFromCharCode.apply(null, codeUnits);
+          codeUnits.length = 0;
+        }
+      }
+      return result;
+    };
+  }());
 
   function parse(str, flags, features) {
     if (!features) {
