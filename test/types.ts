f4e8971642865e257cbbbf940443c8a2a5a6d746
Tom Jenkinson <tom@tjenkinson.me>
2021-12-04 18:20:46 +0000

    update test

--- test/types.ts
+++ test/types.ts
@@ -1,20 +1,29 @@
 import { AstNodeType, parse, RootNode } from "../parser";
 
-let node: RootNode;
+let defaultNode: RootNode;
 let number: number;
 let string: string;
 let type: AstNodeType;
 
-node = parse("", "");
-node = parse("", "", {});
-node = parse("", "", {
+defaultNode = parse("", "");
+defaultNode = parse("", "", {});
+defaultNode = parse("", "", {
   lookbehind: true,
   namedGroups: true,
-  unicodePropertyEscape: true,
   unicodeSet: true,
 });
 
-number = node.range[0];
-number = node.range[1];
-string = node.raw;
-type = node.type;
+// @ts-expect-error
+defaultNode.type === "unicodePropertyEscape";
+
+number = defaultNode.range[0];
+number = defaultNode.range[1];
+string = defaultNode.raw;
+type = defaultNode.type;
+
+let nodeWithUnicodePropertyEscape: RootNode<{ unicodePropertyEscape: true }>;
+nodeWithUnicodePropertyEscape = parse("", "", {
+  unicodePropertyEscape: true,
+});
+
+nodeWithUnicodePropertyEscape.type === "unicodePropertyEscape";
