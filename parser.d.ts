048ae200d1d64c339d7d3cca8d82931f0072c8dd
Tom Jenkinson <tjenkinson@users.noreply.github.com>
2021-12-29 21:50:09 +0000

    Update parser.d.ts

--- parser.d.ts
+++ parser.d.ts
@@ -130,7 +130,7 @@ export type Quantifier<F extends Features = {}> = Base<"quantifier"> & {
   greedy: boolean;
   max?: number;
   min: number;
-  symbol?: string;
+  symbol?: '?' | '*' | '+';
 };
 
 export type Disjunction<F extends Features = {}> = Base<"disjunction"> & {
